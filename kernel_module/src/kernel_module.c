88b30a21d61af6d6a79b278c7dc8c5eae75a6d96
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-05 03:44:20 +0200

    Ro to rw

--- kernel_module/src/kernel_module.c
+++ kernel_module/src/kernel_module.c
@@ -85,12 +85,12 @@ static ssize_t rabb_store(struct kobject *kobj, struct kobj_attribute *attr,cons
 		writel(ctrl, SYKT_GPIO_CTRL_ADDR);
         return count;
 }
-// makra do komunikacjiSSSSSS
+// makra do komunikacji
 
-static struct kobj_attribute raba1_attr = __ATTR_RW(raba1);
-static struct kobj_attribute raba2_attr = __ATTR_RW(raba2);
-static struct kobj_attribute rabw_attr = __ATTR(rabw, 0660, rabw_show, rabw_store);
-static struct kobj_attribute rabl_attr = __ATTR(rabl, 0660, rabl_show, rabl_store);
+static struct kobj_attribute raba1_attr = __ATTR_WO(raba1);
+static struct kobj_attribute raba2_attr = __ATTR_WO(raba2);
+static struct kobj_attribute rabw_attr = __ATTR_RO(rabw);
+static struct kobj_attribute rabl_attr = __ATTR_RO(rabl);
 static struct kobj_attribute rabb_attr = __ATTR_RW(rabb);
 
 // ===================================================================

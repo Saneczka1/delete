3b734ef02d46c4d68e6dbb7a69bbe4f6155f352e
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-08 21:31:22 +0200

    add ishex

--- main.c
+++ main.c
@@ -116,7 +116,7 @@ while (read != 3 && readw == 0 && readl == 0);
     readw = read_from_file(SYSFS_FILE_RES);
     readl = read_from_file(SYSFS_FILE_ONES);
     readb = read_from_file(SYSFS_FILE_STATUS);
-}
+//}
 /*
  while () {
         unsigned int read = read_from_file(SYSFS_FILE_STATUS);

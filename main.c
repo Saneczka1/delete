2a06044437440568e92f2a6595b114d3a3c4c6c4
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-12 22:15:15 +0200

    maybe wass error in read function

--- main.c
+++ main.c
@@ -56,33 +56,19 @@ exit(1);
 }
 int n=read(file, buffer, MAX_BUFFER);
 if(n>0){   
-        buffer[n]='\0';
+     //   buffer[n]='\0';
         
         close(file);
-         exit(2);
+         exit(4);
         return strtoul(buffer, NULL, 16);  // 16 znaczy HEX
     }else{
         printf("Open %s - error %d\n", filePath, errno); 
         close(file);
-        exit(3);
+        exit(5);
     }
 }
 
 /*
-unsigned int read_from_file(char *filePath){
-	char buffer[MAX_BUFFER];
-	int file=open(filePath, O_RDONLY);
-	if(file<0){
-		 printf("Open %s - error number %d\n", filePath, errno);
-		 exit(5);
-	}
-	int n=read(file, buffer, MAX_BUFFER);
-	close(file);
-	return strtoul(buffer, NULL, 16);
-	 
-}
-
-
 int write_to_file(char *filePath, unsigned int input){
 	char buffer[MAX_BUFFER];
 	FILE *file=fopen(filePath, "w");
@@ -105,8 +91,11 @@ int write_to_file(char *filePath, unsigned int input){
 		 printf("Open %s - error number %d\n", filePath, errno);
 		 exit(2);
 	}
+
 	snprintf(buffer, MAX_BUFFER, "%x", input);
+    write(fd_in, buffer, strlen(buffer));
 	int n=write(fd_in, buffer, strlen(buffer));
+
     if(n!=strlen(buffer)){
         printf("Open %s - error number %d\n", filePath, errno);
         close(fd_in);
@@ -188,14 +177,10 @@ int l=0;
         readb = read_from_file(SYSFS_FILE_STATUS);
 
 
-
-
 struct multiplication_result result;
   result.w = readw;
   result.l = readl;
   result.b = readb;
-
-
 return result;
 }
 
@@ -218,36 +203,23 @@ int count_ones(unsigned int n) {
 }
 
 int test_module(){
-
     typedef struct {
     unsigned int a1;
     unsigned int a2;
     unsigned int w;
-    unsigned int num_ones;
-} MyStruct;
-
+    unsigned int num_ones;} MyStruct;
 
 
  MyStruct values[500];
-
  for (int i = 0; i < 500; i++) {
         values[i].a1 = random_in_range(0, 9); // 20 
         values[i].a2 = random_in_range(0, 9); // 20 
         values[i].w = values[i].a1 * values[i].a2;
-        values[i].num_ones = count_ones(values[i].w);}
-
-
-
+        values[i].num_ones = count_ones(values[i].w);
+    }
 
 
 int k=0;
-/*
-unsigned int readw = 0;
-for(int i=0; i<500; i++)
-{
-    readw = read_from_file(SYSFS_FILE_RES);
-    printf("%u", readw);
-}*/
 for(int i=0; i<50; i++){
 struct multiplication_result result = multiply(values[i].a1,values[i].a2);
 if( result.w != values[i].w && result.l != values[i].num_ones)

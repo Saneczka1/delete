b03a2d2674f63b720a21d745f9eb03d4fac69a7e
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-12 18:01:16 +0200

    change dec to hex in result i should have hex now in expected result

--- main.c
+++ main.c
@@ -45,6 +45,40 @@ return 0;
 }
 
 
+string decToHex(int n)
+{
+    
+    string ans = "";
+    
+    while (n != 0) {
+        int rem = 0;
+        char ch;
+        rem = n % 16;
+       
+        if (rem < 10) {
+            ch = rem + 48;
+        }
+        else {
+            ch = rem + 55;
+        }
+        
+        ans += ch;
+        n = n / 16;
+    }
+      
+   
+    int i = 0, j = ans.size() - 1;
+    while(i <= j)
+    {
+      swap(ans[i], ans[j]);
+      i++;
+      j--;
+    }
+    return ans;
+}
+
+
+
 unsigned int read_from_file(char *filePath){
 char buffer[MAX_BUFFER];
 
@@ -195,8 +229,8 @@ int test_module(){
  for (int i = 0; i < 500; i++) {
         values[i].a1 = random_in_range(0, 9); // 20 
         values[i].a2 = random_in_range(0, 9); // 20 
-        values[i].w = values[i].a1 * values[i].a2;
-        values[i].num_ones = count_ones(values[i].w);}
+        values[i].w = decToHex(values[i].a1 * values[i].a2);
+        values[i].num_ones = decToHex(count_ones(values[i].w));}
 
 
 

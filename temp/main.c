25fdbb6754cb2b1bd8e2f59a649c6c8e7d62080f
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-16 14:03:49 +0200

    откат

--- temp/main.c
+++ temp/main.c
@@ -107,20 +107,18 @@ struct multiplication_result multiply(unsigned int arg1, unsigned int arg2){
 
     int k =0;
     int l=0;
-    int m =0;
 	while (l==0) {
       //  unsigned int read = read_from_file(SYSFS_FILE_STATUS);
         readw = read_from_file(SYSFS_FILE_RES);
         readl = read_from_file(SYSFS_FILE_ONES);
         readb = read_from_file(SYSFS_FILE_STATUS);
+        reada1 = read_from_file( SYSFS_FILE_WE1);
+        reada2 = read_from_file( SYSFS_FILE_WE2);
        
-       // reada1 = read_from_file( SYSFS_FILE_WE1);
-       // reada2 = read_from_file( SYSFS_FILE_WE2);
-       
-        if (readb == 3 && readw != 0  && m >1){
+        if (readb == 3 && readw != 0 ){
         l++;}
 
-        if (readb == 0 )
+        if (readb ==0 )
             {
             printf("result cannot be represented in 32 bits");    
             break;
@@ -130,6 +128,7 @@ struct multiplication_result multiply(unsigned int arg1, unsigned int arg2){
         break;
         }
         k++;
+        
     }
 
 

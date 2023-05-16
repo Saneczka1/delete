8c88cc2f4bd9d34abdab2f36079801603ff26cb0
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-05-16 15:01:07 +0200

    zmieniłem if i state

--- temp/gpioemu.v
+++ temp/gpioemu.v
@@ -99,7 +99,7 @@ always @(posedge srd)
 begin
     if (saddress == 16'h0390) 
 	
-       if (B == 11) begin  //jeszcze sprawdzić to
+       if (B == 2'b11) begin  //jeszcze sprawdzić to
 	    sdata_out_s <= W[31:0];
         end
     
